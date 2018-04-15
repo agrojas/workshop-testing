333a9c2bbf121bcb2c2f2ad585166b66f85ac4ba
agrojas <rojasagustin90@gmail.com>
2018-04-15 18:01:55 -0300

    add comment for sections

--- test/test.js
+++ test/test.js
@@ -2,12 +2,19 @@
 var addTwo = require('../index.js');
 
 function testAddTwo() {
+  // 1. ARRANGE
   var x = 5;
   var sum1 = x + 2;
+
+
+  // 2. ACT
   var sum2 = addTwo(x);
 
   console.log('Expect ' + sum1 + ' to equal ' + sum2 + '.');
   
+
+
+  // 3. ASSERT
   if ( sum1 === sum2 ) 
     return console.log('Passed.');
   
